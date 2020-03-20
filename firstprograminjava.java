09d63b4103480ec97ed221d72a2a3cf116224e57
VadivelanDevOps <62283794+VadivelanDevOps@users.noreply.github.com>
2020-03-20 07:26:05 +0530

    Delete firstprograminjava.java

--- firstprograminjava.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.util.*;
-class firstprograminjava
-{
-	public static void main (String args[])
-		{
-			System.out.println("This is my first program in java");
-		}
-}
